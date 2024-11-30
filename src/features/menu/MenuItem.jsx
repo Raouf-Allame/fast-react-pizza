ec44dc59e15d0fb2df4d5281095d7d3fbcbe7c97
raouf <rauofallama@gmail.com>
2024-11-30 12:55:51 -0800

    Update Cart Quantities

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import { formatCurrency } from "../../utils/helpers";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch()
@@ -39,7 +40,12 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-           {isInCart && <DeleteItem pizzaId={id}/>}
+           {isInCart && <div className="flex items-center gap-3 sm:gap-8">
+
+            <UpdateItemQuantity pizzaId={id} currentQuantity={currnetQuantity}/>
+            <DeleteItem pizzaId={id}/>
+            
+            </div>}
 
           {!soldOut && !isInCart && <div className="flex gap-2">
             <Button type="small" onClick={handleClick}>Add to cart</Button>
